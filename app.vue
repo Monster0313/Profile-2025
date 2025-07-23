e3fc959671892311ef24e9b0846a673475544205
Monster0313 <y779380@gmail.com>
2025-07-24 01:48:55 +0800

    feat: 加入title

--- app.vue
+++ app.vue
@@ -9,4 +9,11 @@
 <script setup>
 import Navbar from "./components/navbar/Navbar.vue";
 import Footer from "./components/footer/Footer.vue";
+
+useSeoMeta({
+  title: "小怪｜個人網站",
+  ogTitle: "小怪｜個人網站",
+  description: "這裡是小怪的個人網站，快來看看吧！",
+  ogDescription: "這裡是小怪的個人網站，快來看看吧！",
+});
 </script>
